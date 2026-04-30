d8369f8a980a6fee38ec6bb5249e155a5971d88c
EinirIngi <einiringi12@gmail.com>
2026-04-30 11:35:31 +0000

    Refactor sensor scanning and status retrieval
    Refactor code to define functions for scanning both sensors and getting front status. Update I2C addresses and improve readability.

--- srf02.py
+++ srf02.py
@@ -2,19 +2,37 @@ import time
 from smbus2 import SMBus
 
 i2c_bus = SMBus(1)
-i2c_address = 0x70
+sonic_r = 0x70
+sonic_l = 0x71
 
-while 1:
-    i2c_bus.write_byte_data(i2c_address, 0, 0x51)  # Tell sensor to scan in mm
+def scan_both():
+    i2c_bus.write_byte_data(sonic_r, 0, 0x51)
+    i2c_bus.write_byte_data(sonic_l, 0, 0x51)
 
-    high = i2c_bus.read_byte_data(i2c_address, 2)  # Read the high byte of the value
-    #print(high) # print the value of High byte
+    time.sleep(0.07)
 
-    low = i2c_bus.read_byte_data(i2c_address, 3)  # Read the low byte of the value
-    #print(low) # print the value of low byte
+    high_r = i2c_bus.read_byte_data(sonic_r, 2)
+    low_r  = i2c_bus.read_byte_data(sonic_r, 3)
 
-    current_value = high * 256 + low 
+    high_l = i2c_bus.read_byte_data(sonic_l, 2)
+    low_l  = i2c_bus.read_byte_data(sonic_l, 3)
 
-    print(current_value)
+    r = high_r * 256 + low_r
+    l = high_l * 256 + low_l
 
-    time.sleep(0.1)  # Sleep for some
+    return r, l
+
+def get_front_status(limit=250):
+    '''Skannar og prentar fjarðlægð frá skynjurum og hlut. Skilar B ef að báðir eru blockaðir, R ef að hægri er bara bloockaður, L ef að vinstri er bara blockaður eða C ef að enginn err blockaður. Limit er sjálfkrafa á 250mm en hægt er að breyta'''
+    r_dis, l_dis = scan_both() # Scannar með báðum og gefur fjarðlægð til baka
+
+    print("R:", r_dis, "L:", l_dis) # Prentar fjarlægð beggja skynjara (má taka úr)
+
+    if r_dis <= limit and l_dis <= limit:
+        return "B" # Skilar B ef að báðir eru blockaðir
+    elif r_dis <= limit:
+        return "R" # Skilar R ef að bara hægri er blockaður
+    elif l_dis <= limit:
+        return "L" # Skilar L ef að bara vinstri er blockaður
+    else:
+        return "C" # Ef að ekkert er fyrir skilar hann C sem að gott er að skilgriena sem True fyrir While lykkju
