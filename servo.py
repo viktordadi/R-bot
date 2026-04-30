aee13cc5e00e61877e03171e59c2b7a8af1eb5f4
EinirIngi <einiringi12@gmail.com>
2026-04-30 13:57:41 +0000

    Update servo.py

--- servo.py
+++ servo.py
@@ -8,11 +8,11 @@ def scan(sonic=True):
   kit.servo[1].angle = 90
   while sonic:
     time.sleep(0.5)
-    kit.servo[0].angle = 160
-    kit.servo[1].angle = 20
+    kit.servo[0].angle = 110
+    kit.servo[1].angle = 70
     time.sleep(0.5)
-    kit.servo[0].angle = 30
-    kit.servo[1].angle = 150
+    kit.servo[0].angle = 45
+    kit.servo[1].angle = 135
     
   
 scan()
