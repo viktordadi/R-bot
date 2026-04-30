48f14a2245aa0caa35bb9a3d97c639c8758d0eec
EinirIngi <einiringi12@gmail.com>
2026-04-30 09:36:11 +0000

    Create scan function for servo control
    Refactor servo movement logic into a scan function.

--- servo.py
+++ servo.py
@@ -2,10 +2,19 @@ import time
 from adafruit_servokit import ServoKit
 # Initialize for 8 channels
 kit = ServoKit(channels=8)
-while True:
-  print("Moving to 180")
-  kit.servo[0].angle = 180
-  time.sleep(1)
-  print("Moving to 0")
+
+def scan(sonic=True):
   kit.servo[0].angle = 0
-  time.sleep(1)
+  kit.servo[1].angle = 0
+  while True:
+    if sonic:
+      time.sleep(0.5)
+      kit.servo[0].angle = 45
+      kit.servo[1].angle = -45
+      time.sleep(0.5)
+      kit.servo[0].angle = -30
+      kit.servo[1].angle = 30
+    else:
+      break
+    
+  
