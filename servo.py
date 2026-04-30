319ab6290ed3008b3362f278799d351b9f5d32f2
EinirIngi <einiringi12@gmail.com>
2026-04-30 09:43:19 +0000

    Update servo.py

--- servo.py
+++ servo.py
@@ -6,16 +6,13 @@ kit = ServoKit(channels=8)
 def scan(sonic=True):
   kit.servo[0].angle = 90
   kit.servo[1].angle = 90
-  while True:
-    if sonic:
-      time.sleep(0.5)
-      kit.servo[0].angle = 45
-      kit.servo[1].angle = 180-45
-      time.sleep(0.5)
-      kit.servo[0].angle = 180-30
-      kit.servo[1].angle = 30
-    else:
-      break
+  while sonic:
+    time.sleep(0.5)
+    kit.servo[0].angle = 45
+    kit.servo[1].angle = 135
+    time.sleep(0.5)
+    kit.servo[0].angle = 150
+    kit.servo[1].angle = 30
     
   
 scan()
