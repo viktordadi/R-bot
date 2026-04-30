c11329fd50b25d27da6f2d51c98a229b1e86c117
EinirIngi <einiringi12@gmail.com>
2026-04-30 14:05:33 +0000

    Fix indentation in scan function

--- servo.py
+++ servo.py
@@ -6,13 +6,13 @@ kit = ServoKit(channels=8)
 def scan(sonic=True):
   kit.servo[0].angle = 90
   kit.servo[1].angle = 90
-    while sonic:
-      time.sleep(0.5)
-      kit.servo[0].angle = 110
-      kit.servo[1].angle = 70
-      time.sleep(0.5)
-      kit.servo[0].angle = 45
-      kit.servo[1].angle = 135
-    
+  while sonic:
+    time.sleep(0.5)
+    kit.servo[0].angle = 110
+    kit.servo[1].angle = 70
+    time.sleep(0.5)
+    kit.servo[0].angle = 45
+    kit.servo[1].angle = 135
+  
   
 scan()
