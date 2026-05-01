fb4d8300afd29fa23d63182206a7b13d1932c2fc
EinirIngi <einiringi12@gmail.com>
2026-05-01 13:38:31 +0000

    Simplify scan function by removing while loop
    Refactor scan function to remove while loop and simplify servo movements.

--- servo.py
+++ servo.py
@@ -6,13 +6,12 @@ kit = ServoKit(channels=8)
 def scan(sonic=True):
   kit.servo[0].angle = 90
   kit.servo[1].angle = 90
-  while sonic:
-    time.sleep(0.5)
-    kit.servo[0].angle = 105
-    kit.servo[1].angle = 75
-    time.sleep(0.5)
-    kit.servo[0].angle = 70
-    kit.servo[1].angle = 110
+  time.sleep(0.5)
+  kit.servo[0].angle = 105
+  kit.servo[1].angle = 75
+  time.sleep(0.5)
+  kit.servo[0].angle = 70
+  kit.servo[1].angle = 110
   
   
 scan()
