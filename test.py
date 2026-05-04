cf1e9592603d6a23cf51b82087d545cda32f7379
viktordadi <viktordadiegg@gmail.com>
2026-05-04 14:08:23 +0000

    Update test.py

--- test.py
+++ test.py
@@ -1,15 +1,30 @@
 import smbus
 import time
 
-bus = smbus.SMBus(1)
 I2C_ADDRESS = 0x50
+bus = smbus.SMBus(1)
 
-def send(data):
-    print("Sending:", data)
+def send_motors(left, right):
+    left = max(-255, min(255, int(left)))
+    right = max(-255, min(255, int(right)))
+
+    left_speed = abs(left)
+    left_sign = 0 if left >= 0 else 1
+
+    right_speed = abs(right)
+    right_sign = 0 if right >= 0 else 1
+
+    data = [left_speed, left_sign, right_speed, right_sign]
+    print(data)
     bus.write_i2c_block_data(I2C_ADDRESS, 0x00, data)
 
-# Old 2-motor format
-send([100, 0, 100, 0])
-time.sleep(1)
+print("Forward")
+send_motors(100, -100)
+time.sleep(2)
 
-send([0, 0, 0, 0])
+print("Backward")
+send_motors(-100, 100)
+time.sleep(2)
+
+print("Stop")
+send_motors(0, 0)
