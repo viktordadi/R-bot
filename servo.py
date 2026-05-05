a2d2885f90d16bc6e7469ec7a138cdeb77bd7a3f
EinirIngi <einiringi12@gmail.com>
2026-05-05 11:58:38 +0000

    Refactor servo control by removing detect function
    Removed the detect function and updated the scan function to adjust servo angles with longer sleep durations.

--- servo.py
+++ servo.py
@@ -3,17 +3,12 @@ from adafruit_servokit import ServoKit
 # Initialize for 8 channels
 kit = ServoKit(channels=8)
 
-def detect():
-  kit.servo[0].angle = 110
-  kit.servo[1].angle = 70
-
 def scan(sonic=True):
   kit.servo[0].angle = 90
   kit.servo[1].angle = 90
-  time.sleep(0.3)
-  kit.servo[0].angle = 110
-  kit.servo[1].angle = 70
-  time.sleep(0.3)
-  kit.servo[0].angle = 80
-  kit.servo[1].angle = 100
-  time.sleep(0.3)
+  time.sleep(0.5)
+  kit.servo[0].angle = 105
+  kit.servo[1].angle = 75
+  time.sleep(0.5)
+  kit.servo[0].angle = 70
+  kit.servo[1].angle = 110
