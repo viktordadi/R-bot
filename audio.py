a42941610c910e56d00421f89d2ccf34221b6c5c
EinirIngi <einiringi12@gmail.com>
2026-05-05 10:30:16 +0000

    Implement audio playback functions
    Added functions to play different audio tracks using pygame.

--- audio.py
+++ audio.py
@@ -1,1 +1,17 @@
+import pygame
 
+pygame.mixer.init()
+
+
+def fireball():
+  pygame.mixer.music.load("fireball.mp3")
+  pygame.mixer.music.play()
+
+def rain_over_me():
+  pygame.mixer.music.load("rain_over_me.mp3")
+  pygame.mixer.music.play()
+
+def mr_worldwide():
+  pygame.mixer.music.load("mr_worldwide.mp3")
+  pygame.mixer.music.play()
+  
