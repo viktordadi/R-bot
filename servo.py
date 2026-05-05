238f0014ec6943edf461a6cba2842b59b021836e
EinirIngi <einiringi12@gmail.com>
2026-05-05 12:54:16 +0000

    Refactor servo functions and add scan method
    Refactored servo control functions to include scanning functionality.

--- servo.py
+++ servo.py
@@ -3,24 +3,14 @@ from adafruit_servokit import ServoKit
 
 kit = ServoKit(channels=8)
 
-def center():
+def scan(sonic=True):
     kit.servo[0].angle = 90
     kit.servo[1].angle = 90
+    time.sleep(0.5)
 
-def look_right():
     kit.servo[0].angle = 105
     kit.servo[1].angle = 75
+    time.sleep(0.5)
 
-def look_left():
     kit.servo[0].angle = 70
     kit.servo[1].angle = 110
-
-def move_and_wait(position):
-    if position == "center":
-        center()
-    elif position == "right":
-        look_right()
-    elif position == "left":
-        look_left()
-
-    time.sleep(0.15)
