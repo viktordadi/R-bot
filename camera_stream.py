4c049c068c46f8669d6ce7618293a41feacd9311
thorleifur123 <thorleifurel@gmail.com>
2026-05-07 13:18:03 +0000

    Update camera_stream.py

--- camera_stream.py
+++ camera_stream.py
@@ -2,23 +2,21 @@ import io
 import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from picamera2 import Picamera2
+from picamera2.encoders import MJPEGEncoder
+from picamera2.outputs import FileOutput
 
 camera = Picamera2()
 camera.configure(camera.create_video_configuration(main={"size": (640, 480)}))
-camera.start()
 
 frame_lock = threading.Lock()
 latest_frame = b""
+server = None
 
-def capture_loop():
-    global latest_frame
-    while True:
-        stream = io.BytesIO()
-        camera.capture_file(stream, format="jpeg")
+class StreamOutput(io.BufferedIOBase):
+    def write(self, buf):
+        global latest_frame
         with frame_lock:
-            latest_frame = stream.getvalue()
-
-threading.Thread(target=capture_loop, daemon=True).start()
+            latest_frame = buf
 
 class StreamHandler(BaseHTTPRequestHandler):
     def do_GET(self):
@@ -28,14 +26,26 @@ class StreamHandler(BaseHTTPRequestHandler):
         while True:
             with frame_lock:
                 frame = latest_frame
-            self.wfile.write(b"--frame\r\n")
-            self.wfile.write(b"Content-Type: image/jpeg\r\n\r\n")
-            self.wfile.write(frame)
-            self.wfile.write(b"\r\n")
+            if frame:
+                self.wfile.write(b"--frame\r\n")
+                self.wfile.write(b"Content-Type: image/jpeg\r\n\r\n")
+                self.wfile.write(frame)
+                self.wfile.write(b"\r\n")
 
     def log_message(self, format, *args):
-        pass  # þaggar HTTP logs
+        pass
 
-server = HTTPServer(("0.0.0.0", 8080), StreamHandler)
-print("Stream á: http://<IP á Pi>:8080")
-server.serve_forever()
+def start():
+    global server
+    output = StreamOutput()
+    camera.start_recording(MJPEGEncoder(), FileOutput(output))
+    server = HTTPServer(("0.0.0.0", 8080), StreamHandler)
+    threading.Thread(target=server.serve_forever, daemon=True).start()
+    print("Stream á: http://<IP á Pi>:8080")
+
+def stop():
+    global server
+    camera.stop_recording()
+    if server:
+        server.shutdown()
+        server = None
