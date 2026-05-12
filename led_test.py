e1d4f7c34c1f7c2544e5c628dbac3e1b1373ddf9
viktordadi <viktordadiegg@gmail.com>
2026-05-12 16:55:21 +0000

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -2,23 +2,27 @@ import board
 import neopixel
 import time
 
-NUM_LEDS = 26
+NUM_LEDS = 60  # change this to your real LED count
 
 pixels = neopixel.NeoPixel(
     board.D18,
     NUM_LEDS,
-    brightness=0.3,
-    auto_write=True,
+    brightness=0.2,
+    auto_write=False,
     pixel_order=neopixel.GRB
 )
 
 pixels.fill((255, 0, 0))
-time.sleep(1)
+pixels.show()
+time.sleep(2)
 
 pixels.fill((0, 255, 0))
-time.sleep(1)
+pixels.show()
+time.sleep(2)
 
 pixels.fill((0, 0, 255))
-time.sleep(1)
+pixels.show()
+time.sleep(2)
 
 pixels.fill((0, 0, 0))
+pixels.show()
