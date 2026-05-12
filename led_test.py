18abab063591296599cfa64cad6c1a22cbe0b169
viktordadi <viktordadiegg@gmail.com>
2026-05-12 16:49:17 +0000

    Create led_test.py

---
+++ led_test.py
@@ -0,0 +1,24 @@
+import board
+import neopixel
+import time
+
+NUM_LEDS = 10
+
+pixels = neopixel.NeoPixel(
+    board.D18,
+    NUM_LEDS,
+    brightness=0.3,
+    auto_write=True,
+    pixel_order=neopixel.GRB
+)
+
+pixels.fill((255, 0, 0))
+time.sleep(1)
+
+pixels.fill((0, 255, 0))
+time.sleep(1)
+
+pixels.fill((0, 0, 255))
+time.sleep(1)
+
+pixels.fill((0, 0, 0))
