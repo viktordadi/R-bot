7585a86eeca1e5c968b347d5d1e644bd6fc8e5d0
viktordadi <viktordadiegg@gmail.com>
2026-05-12 16:57:26 +0000

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -7,7 +7,7 @@ NUM_LEDS = 60  # change this to your real LED count
 pixels = neopixel.NeoPixel(
     board.D18,
     NUM_LEDS,
-    brightness=0.05,
+    brightness=0.03,
     auto_write=False,
     pixel_order=neopixel.GRB
 )
