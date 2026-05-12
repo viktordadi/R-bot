b1a648a6113c78aba9cc1d58498c2fcdc9d87424
viktordadi <viktordadiegg@gmail.com>
2026-05-12 17:11:13 +0000

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -7,14 +7,14 @@ NUM_LEDS = 60
 pixels = neopixel.NeoPixel(
     board.D18,
     NUM_LEDS,
-    brightness=0.01,
+    brightness=0.05,
     auto_write=False,
     pixel_order=neopixel.GRB
 )
 
-while True:
-    for i in range(NUM_LEDS):
-        pixels.fill((0, 0, 0))
-        pixels[i] = (255, 0, 0)
-        pixels.show()
-        time.sleep(0.5)
+pixels.fill((255, 0, 0))
+pixels.show()
+time.sleep(3)
+
+pixels.fill((0, 0, 0))
+pixels.show()
